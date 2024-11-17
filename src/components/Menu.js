43f18a0b222dd971dfc3543b5ddfdf69131261df
fredi1574 <fredi1574@gmail.com>
2024-11-17 16:02:45 +0200

    Fixed the highlighting of the current page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -19,16 +19,8 @@ const menuItems = [
     href: "/activities",
   },
   { icon: "/menuIcons/courses.png", label: "Courses", href: "/courses" },
-  {
-    icon: "/menuIcons/lecturers.png",
-    label: "Lecturers",
-    href: "/lecturers",
-  },
-  {
-    icon: "/menuIcons/questionnaire.png",
-    label: "Surveys",
-    href: "/surveys",
-  },
+  { icon: "/menuIcons/lecturers.png", label: "Lecturers", href: "/lecturers" },
+  { icon: "/menuIcons/questionnaire.png", label: "Surveys", href: "/surveys" },
 ];
 
 const Menu = () => {
@@ -39,25 +31,28 @@ const Menu = () => {
       <div className="mt-4 flex h-[87dvh] flex-col text-sm">
         {/* Menu items */}
         <div className="flex-1">
-          {menuItems.map((item) => (
-            <Tooltip key={item.label} delayDuration={50}>
-              <TooltipTrigger asChild>
-                <Link
-                  href={item.href}
-                  className={
-                    `group relative flex justify-center gap-4 py-3 pl-1 text-gray-500 transition hover:brightness-0 lg:justify-start lg:pl-3` +
-                    (pathname === item.href ? " brightness-0" : "")
-                  }
-                >
-                  <Image src={item.icon} alt="icon" width={25} height={25} />
-                  <span className="hidden lg:block">{item.label}</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right" className="lg:hidden">
-                <span>{item.label}</span>
-              </TooltipContent>
-            </Tooltip>
-          ))}
+          {menuItems.map((item) => {
+            const isActive = pathname.startsWith(item.href);
+            return (
+              <Tooltip key={item.label} delayDuration={50}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.href}
+                    className={
+                      `group relative flex justify-center gap-4 py-3 pl-1 text-gray-500 transition hover:brightness-0 lg:justify-start lg:pl-3` +
+                      (isActive ? " brightness-0" : "")
+                    }
+                  >
+                    <Image src={item.icon} alt="icon" width={25} height={25} />
+                    <span className="hidden lg:block">{item.label}</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right" className="lg:hidden">
+                  <span>{item.label}</span>
+                </TooltipContent>
+              </Tooltip>
+            );
+          })}
         </div>
       </div>
     </TooltipProvider>
