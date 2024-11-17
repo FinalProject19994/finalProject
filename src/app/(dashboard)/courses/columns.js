48c74713e597a3e8b5cc8d8f05289e954d5cd344
fredi1574 <fredi1574@gmail.com>
2024-11-17 15:59:42 +0200

    Removed the arrows icon, Created a new course form

--- src/app/(dashboard)/courses/columns.js
+++ src/app/(dashboard)/courses/columns.js
@@ -7,7 +7,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
 export const columns = [
   {
--- src/app/(dashboard)/courses/page.js
+++ src/app/(dashboard)/courses/page.js
@@ -17,8 +17,12 @@ import { collection, getDocs } from "firebase/firestore";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { columns } from "./columns";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -42,68 +46,21 @@ const Page = () => {
 
   return (
     <div className="flex h-[90dvh] flex-col gap-4 px-4 md:flex-row">
-      {/* ADD ACTIVITY MODAL */}
-      <Dialog>
-        <DialogTrigger className="flex h-8 w-8 items-center justify-center rounded-full hover:border hover:border-gray-500">
-          <Image src="/menuIcons/plus.png" alt="add" width={14} height={14} />
-        </DialogTrigger>
-        <DialogContent className="text-sm">
-          <DialogHeader>
-            <DialogTitle className="text-center">ADD COURSE</DialogTitle>
-          </DialogHeader>
-          <input
-            type="text"
-            placeholder="Enter a title..."
-            className="rounded-md border p-2 text-gray-700 outline-none"
-          />
-          <MultipleSelector
-            // TODO: add skills from the database and remove the local array
-            options={[
-              { id: 1, label: "Communication" },
-              { id: 2, label: "Teamwork" },
-              { id: 3, label: "Problem Solving" },
-              { id: 4, label: "Leadership" },
-              { id: 5, label: "Creativity" },
-              { id: 6, label: "Adaptability" },
-              { id: 7, label: "Critical Thinking" },
-              { id: 8, label: "Interpersonal Skills" },
-            ]}
-            // TODO: add Lecturers from the database
-          />
-          <input
-            type="number"
-            placeholder="Week number..."
-            max={13}
-            min={1}
-            className="w-1/2 rounded-md border p-2 text-gray-700 outline-none"
-          />
-          <textarea
-            placeholder="Enter description..."
-            className="rounded-md border p-2 text-gray-700 outline-none"
-          />
-          <textarea
-            placeholder="Enter reflection..."
-            className="rounded-md border p-2 text-gray-700 outline-none"
-          />
-          <input
-            type="file"
-            accept="image/*"
-            className="rounded-md border border-gray-200 text-sm text-gray-700 file:mr-2 file:border-0 file:bg-gray-300 file:px-2 file:py-3 file:text-gray-700 focus:z-10"
-          />
-          {/* <Link
-              href="/surveys"
-              className="text-primary_purple hover:underline"
-            >
-              Surveys
-            </Link> */}
-          <button className="w-full rounded-md bg-primary_purple p-2 text-white">
-            Add
-          </button>
-        </DialogContent>
-      </Dialog>
-
       {/* LEFT - COURSES TABLE */}
       <div className="w-3/5 overflow-y-scroll rounded-md bg-white p-4 shadow-md">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold text-gray-600">Courses</h1>
+          <button
+            onClick={() => {
+              router.push("/courses/new");
+            }}
+            className="flex gap-2 rounded-md border border-gray-200 p-2 duration-150 hover:bg-gray-100"
+          >
+            <Plus />
+            {/* TODO: Center the text vertically */}
+            <span className="text-sm text-gray-700">Create new course</span>
+          </button>
+        </div>
         {loading ? <Loader /> : <DataTable data={courses} columns={columns} />}
       </div>
 
