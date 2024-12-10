6a9ac1e5b24fe9d8882ae5d2f60c6f44d326dd37
fredi1574 <fredi1574@gmail.com>
2024-12-10 14:29:55 +0200

    Change the fetching to realtime snapShots and introduced the database

--- src/app/(dashboard)/homepage/page.js
+++ src/app/(dashboard)/homepage/page.js
@@ -2,21 +2,36 @@
 import EventCalendar from "@/components/EventCalendar";
 import Chart from "@/components/Chart";
 import ForceDirectedGraph from "@/components/ForceDirectedGraph";
-import { nodes, links } from "@/lib/data";
+// import { nodes, links } from "@/lib/data";
 import Legend from "@/components/ui/Legend";
+import { useEffect } from "react";
+import { fetchGraphData, prepareGraphData } from "@/lib/fetchGraphData";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [graphData, setGraphData] = useState({ nodes: [], links: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const rawData = await fetchGraphData();
+      const processedData = prepareGraphData(rawData);
+      setGraphData(processedData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex h-[90dvh] flex-col gap-4 px-4 text-gray-500 md:flex-row">
       {/* LEFT */}
-      <div className="flex flex-col gap-4 text-3xl md:w-3/4">
-        <div className="relative h-2/3 w-full rounded-md bg-white shadow-md">
+      <div className="flex flex-col gap-4 text-3xl md:w-4/5">
+        <div className="relative h-screen w-full rounded-md bg-white shadow-md md:h-2/3">
           <div className="absolute left-2 top-0 z-10">
             <h2 className="h-fit bg-white text-lg font-semibold">Overview</h2>
             <Legend />
           </div>
 
-          <ForceDirectedGraph nodes={nodes} links={links} />
+          <ForceDirectedGraph nodes={graphData.nodes} links={graphData.links} />
         </div>
 
         {/* LEFT - BOTTOM (Admin only) */}
@@ -34,7 +49,7 @@ const HomePage = () => {
       </div>
 
       {/* RIGHT */}
-      <div className="flex w-full flex-col md:w-1/4">
+      <div className="flex w-full flex-col md:w-1/5">
         <EventCalendar />
       </div>
     </div>
