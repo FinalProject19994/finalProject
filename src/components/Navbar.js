a59e0357d4003878fb761f89f1f2c23de9c77630
fredi1574 <fredi1574@gmail.com>
2024-12-10 21:07:50 +0200

    Removed the search input

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,21 +39,6 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between p-4">
-      {/* Search Bar */}
-      <div className="hidden items-center gap-2 rounded-full border border-gray-500 bg-white p-2 text-xs text-gray-500 md:flex">
-        <Image
-          src="/menuIcons/search.png"
-          alt="search"
-          width={14}
-          height={14}
-        />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="hidden outline-none md:flex"
-        />
-      </div>
-
       {/* Avatar and user */}
       <div className="ml-auto flex items-center gap-4">
         <div className="flex flex-col text-gray-500">
