75a672465ad669b206f2c25a93bfc410b2455c7f
fredi1574 <fredi1574@gmail.com>
2024-12-15 16:18:58 +0200

    Implemented a search function in the tables

---
+++ src/components/SearchableTable.js
@@ -0,0 +1,40 @@
+"use client";
+import DataTable from "@/components/data-table";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Search } from "lucide-react";
+
+export function SearchableTable({ columns, data, handleRowSelect, page }) {
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const filtered = data.filter((item) =>
+      Object.values(item).some(
+        (value) =>
+          typeof value === "string" && value.toLowerCase().includes(searchTerm),
+      ),
+    );
+    setFilteredData(filtered);
+  };
+
+  return (
+    <div className="">
+      <div className="relative top-14 flex w-1/2 rounded-md border border-gray-200 p-2 text-sm">
+        <Search className="mr-2 opacity-50" />
+        <input
+          type="search"
+          placeholder="Search skills..."
+          onChange={handleSearch}
+          className="w-full outline-none"
+        />
+      </div>
+      <DataTable
+        columns={columns}
+        data={filteredData}
+        handleRowSelect={handleRowSelect}
+        page={page}
+      />
+    </div>
+  );
+}
