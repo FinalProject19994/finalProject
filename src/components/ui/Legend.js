8cf571d81f37be44971adbd2e3006c8e2d11c0b3
fredi1574 <fredi1574@gmail.com>
2024-12-21 15:56:35 +0200

    Revamped the Component

--- src/components/ui/Legend.js
+++ src/components/ui/Legend.js
@@ -1,19 +1,59 @@
-import React from "react";
-
-const Legend = () => {
+const Legend = ({ header }) => {
   return (
-    <div className="absolute left-2 top-10 bg-white">
-      <div className="flex items-center gap-2 text-sm">
-        <div className="h-2 w-2 bg-primary_purple_table"></div>
-        <p>Courses</p>
+    <div className="absolute left-0 top-0">
+      <p className="relative top-2 mb-2 bg-white text-3xl font-bold text-gray-600">
+        {header}
+      </p>
+      <div className="my-4 bg-white text-xs">
+        <h6 className="underline">Node types</h6>
+        <div className="flex items-center gap-2">
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <polygon points="6,1 11,6 6,11 1,6" fill="#888" />
+          </svg>
+          <p>Course</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="ml-0.5 h-2 w-2 bg-[#888]"></div>
+          <p>Activity</p>
+        </div>
+        <div className="ml-0.5 flex items-center gap-2">
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="6" cy="6" r="6" fill="#888" />
+          </svg>
+          <p>Skill</p>
+        </div>
       </div>
-      <div className="flex items-center gap-2 text-sm">
-        <div className="h-2 w-2 bg-primary_lightblue"></div>
-        <p>Activities</p>
-      </div>
-      <div className="flex items-center gap-2 text-sm">
-        <div className="h-2 w-2 bg-primary_green"></div>
-        <p>Skills</p>
+
+      <div className="bg-white text-xs">
+        <h6 className="underline">Skill categories</h6>
+        <div className="flex items-center gap-2">
+          <div className="h-2 w-2 bg-primary_purple_table"></div>
+          <p>Professional self</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="h-2 w-2 bg-primary_lightblue"></div>
+          <p>Thinking development</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="h-2 w-2 bg-primary_green"></div>
+          <p>Mindset</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="h-2 w-2 bg-primary_orange"></div>
+          <p>Emotional quotient</p>
+        </div>
       </div>
     </div>
   );
