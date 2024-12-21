f0811714769b5520552a7e63c48cb84e127d432c
fredi1574 <fredi1574@gmail.com>
2024-12-21 15:54:07 +0200

    Added searchable tables

--- src/components/SearchableTable.js
+++ src/components/SearchableTable.js
@@ -1,11 +1,16 @@
 "use client";
 import DataTable from "@/components/data-table";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Search } from "lucide-react";
 
 export function SearchableTable({ columns, data, handleRowSelect, page }) {
-  const [filteredData, setFilteredData] = useState(data);
+  const [filteredData, setFilteredData] = useState([]);
+
+  // Update filteredData when data prop changes
+  useEffect(() => {
+    setFilteredData(data);
+  }, [data]);
 
   const handleSearch = (event) => {
     const searchTerm = event.target.value.toLowerCase();
@@ -19,12 +24,12 @@ export function SearchableTable({ columns, data, handleRowSelect, page }) {
   };
 
   return (
-    <div className="">
+    <div>
       <div className="relative top-14 flex w-1/2 rounded-md border border-gray-200 p-2 text-sm">
         <Search className="mr-2 opacity-50" />
         <input
           type="search"
-          placeholder="Search skills..."
+          placeholder={`Search ${page}...`}
           onChange={handleSearch}
           className="w-full outline-none"
         />
