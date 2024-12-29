d9684a43b30425cf1a1e7f8b24463940507d9c6d
fredi1574 <fredi1574@gmail.com>
2024-12-29 12:37:53 +0200

    Fixed the issue when a user gets a peek even when not logged in

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -6,6 +6,7 @@ import Loader from "@/components/ui/Loader";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+
 const DashboardLayout = ({ children }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
@@ -14,18 +15,19 @@ const DashboardLayout = ({ children }) => {
   useEffect(() => {
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
+      if (user) {
+        setUser(user);
+      } else {
+        router.push("/");
+      }
       setLoading(false);
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [router]);
 
-  if (loading) {
+  if (loading || !user) {
     return <Loader />;
-  }
-  if (!user) {
-    router.push("/");
   }
 
   return (
