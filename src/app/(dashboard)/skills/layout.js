f93f1888b30337049a8cec6fb5d0532fb5ef3323
fredi1574 <fredi1574@gmail.com>
2025-01-11 15:58:50 +0200

    Changed the layout of the page

--- src/app/(dashboard)/skills/layout.js
+++ src/app/(dashboard)/skills/layout.js
@@ -7,10 +7,10 @@ const Page = ({ children }) => {
     <SelectedNodeIdProvider>
       <div className="my-2 flex h-[98%] flex-col gap-4 px-4 md:flex-row">
         {/* LEFT */}
-        <div className="h-[98%] w-full md:w-1/2">{children}</div>
+        <div className="h-[98%] w-full md:w-[60%]">{children}</div>
 
         {/* RIGHT */}
-        <div className="flex h-full rounded-md bg-white shadow-md dark:bg-gray-500 md:w-1/2">
+        <div className="flex h-full rounded-md bg-white shadow-md dark:bg-gray-500 md:w-[40%]">
           <SkillsGraph />
         </div>
       </div>
